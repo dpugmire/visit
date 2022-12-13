b0dee44f24b2ddd09ac0f6a176b7af355ea1945a
Dave Pugmire <dpugmire@gmail.com>
2022-12-13 07:14:05 -0500

    Add FindFides.cmake

---
+++ src/CMake/FindFides.cmake
@@ -0,0 +1,12 @@
+# Copyright (c) Lawrence Livermore National Security, LLC and other VisIt
+# Project developers.  See the top-level LICENSE file for dates and other
+# details.  No copyright assignment is required to contribute to VisIt.
+
+#****************************************************************************
+# Modifications:
+#
+#****************************************************************************/
+
+# Use the FIDES_DIR hint from the config-site .cmake file
+
+SET_UP_THIRD_PARTY(Fides LIBS fides)
